ef98fc4ecb7af0f1e6d6fd6486e83c61099a4b1e
unknown <k.k.m.d.piumini@gmail.com>
2023-10-02 16:09:28 +0530

    modified database

--- src/main/resources/sql/query.sql
+++ src/main/resources/sql/query.sql
@@ -7,24 +7,24 @@ CREATE TABLE category(
     type varchar(50) not null,
     description text not null
 );
+
 CREATE TABLE customer(
-    id varchar(15),
-    national_id varchar(15),
+    id varchar(15) PRIMARY KEY,
+    national_id varchar(15) UNIQUE NOT NULL,
     first_name varchar(50)not null,
     middle_name varchar(50)not null,
     last_name varchar(50),
     address text not null,
-    phone_num varchar(15) not null,
-    PRIMARY KEY(id,national_id)
+    phone_num varchar(15) not null
 );
+
 CREATE TABLE car(
-    car_id varchar(15),
-    number varchar(15),
+    car_id varchar(15) PRIMARY KEY,
+    number varchar(15) UNIQUE NOT NULL,
     typeId varchar(15),
     year INT(4)not null,
     brand varchar(25)not null,
     colour varchar(30)not null,
-    PRIMARY KEY(car_id,number),
     FOREIGN KEY (typeId)REFERENCES category(typeId)
 );
 
@@ -33,30 +33,28 @@ CREATE TABLE rent(
     id varchar(15),
     car_id varchar(15),
     current_date_time TIMESTAMP,
-    due_date
+   // due_date
     FOREIGN KEY(id) REFERENCES customer (id),
     FOREIGN KEY(car_id) REFERENCES car(car_id)
 );
 
 CREATE TABLE user(
-    user_id varchar(30),
-    employee_id varchar(30),
+    user_id varchar(30) PRIMARY KEY,
+    employee_id varchar(30) ,
     user_name varchar(50),
     password varchar(200),
-    PRIMARY KEY(user_id,employee_id),
     FOREIGN KEY(employee_id) REFERENCES employee(employee_id)
 );
 
 CREATE TABLE employee(
-    employee_id varchar(30),
-    national_id varchar(30),
+    employee_id varchar(30)PRIMARY KEY,
+    national_id varchar(30) UNIQUE NOT NULL,
     first_name varchar(50) not null,
     last_name varchar(50)not null,
     DOB varchar(20)not null,
     address text not null,
     phone_num varchar(50)not null,
-    salary varchar(20)not null,
-    PRIMARY KEY(employee_id,national_id),
+    salary varchar(20)not null
 );
 
 
